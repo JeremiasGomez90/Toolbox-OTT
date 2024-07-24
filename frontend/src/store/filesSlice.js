e31cb6c5a7c9ef991a7850a0fa3ad99de11f6bdc
Jeremias Gomez <jeremiasgomez990@gmail.com>
2024-07-24 03:41:56 -0300

    fix wrong filename response

--- frontend/src/store/filesSlice.js
+++ frontend/src/store/filesSlice.js
@@ -1,26 +1,32 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchFiles = createAsyncThunk('files/fetchFiles', async () => {
+export const fetchFiles = createAsyncThunk("files/fetchFiles", async () => {
   let search = window.location.search;
   let params = new URLSearchParams(search);
-  let fileName = params.get('fileName');
+  let fileName = params.get("fileName");
   if (fileName) {
-    const file = await fetch(`http://localhost:5000/files/data?fileName=${fileName}`).then(res => res.json());
+    const file = await fetch(
+      `http://localhost:5000/files/data?fileName=${fileName}`
+    )
+      .then((res) => res.json())
+      .catch(() => []);
     return file;
   }
-  const files = await fetch("http://localhost:5000/files/data").then(res => res.json());
+  const files = await fetch("http://localhost:5000/files/data")
+    .then((res) => res.json())
+    .catch(() => []);
   return files;
 });
 
 export const fileSlice = createSlice({
-  name: 'files',
+  name: "files",
   initialState: {
     files: [],
     loading: true,
     error: null,
   },
   reducers: {},
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder
       .addCase(fetchFiles.pending, (state) => {
         state.loading = true;
@@ -34,6 +40,6 @@ export const fileSlice = createSlice({
         state.error = action.error.message;
       });
   },
-})
+});
 
-export default fileSlice.reducer
+export default fileSlice.reducer;
