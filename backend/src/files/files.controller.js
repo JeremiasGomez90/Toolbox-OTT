8449275d49dac3201fd85d212a8e4c8d9cb3ea20
Jeremias Gomez <jeremiasgomez990@gmail.com>
2024-07-24 03:37:01 -0300

    minor fix

--- backend/src/files/files.controller.js
+++ backend/src/files/files.controller.js
@@ -5,14 +5,16 @@ export async function getFilesData(req, res) {
     const { fileName } = req.query;
     const files = await getFiles();
     if (fileName) {
-      if (!files.includes(fileName)) return res.status(404);
+      if (!files.includes(fileName)) return res.status(404).send([]);
       const row = await formatFileData([fileName]);
       return res.status(200).send(row);
     }
     const rows = await formatFileData(files);
     res.status(200).send(rows);
   } catch (error) {
-    res.status(400).json({ error: true, message: "An error was occurred. Msg: " + error });
+    res
+      .status(400)
+      .json({ error: true, message: "An error was occurred. Msg: " + error });
   }
 }
 
@@ -21,6 +23,8 @@ export async function getFilesList(req, res) {
     const files = await getFiles();
     res.status(200).send({ files });
   } catch (error) {
-    res.status(400).json({ error: true, message: "An error was occurred. Msg: " + error });
+    res
+      .status(400)
+      .json({ error: true, message: "An error was occurred. Msg: " + error });
   }
 }
